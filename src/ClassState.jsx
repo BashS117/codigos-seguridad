e7d8ab446eadb9efee0b6d146233e678f8474da2
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-09-20 12:15:00 -0500

    ciclo de vida en react.component

--- src/ClassState.jsx
+++ src/ClassState.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component{
 constructor(props){
     super(props);
     this.state={
-        error: true,
+        error: false,
+        loading: false,
         }
+    }
+
+    // UNSAFE_componentWillMount(){
+
+    // }
+   
+    componentDidUpdate(){
+
+        if(!!this.state.loading){
+            setTimeout(()=>{
+                console.log("Haciendo la validacion")
+                this.setState({loading: false});
+                console.log("Terminando la validacion")
+            },3000)
+            
+        } 
     }
 
     render() {
@@ -17,11 +35,16 @@ constructor(props){
                 {this.state.error && (
                     <p>Error: el codigo es incorrecto</p>
                 )}
+                {this.state.loading && (
+                    <Loading/>
+                )}
+
                 <input placeholder="Codigo de seguridad" />
                 <button
                 onClick={()=>
                     // this.setState({error: !this.state.error })}
-                    this.setState(prevState=>({error: !prevState .error }))}
+                    // this.setState(prevState=>({error: !prevState .error }))}
+                    this.setState({loading: true })}
                 >Comprobar</button>
             </div>
         )
