fa3cbd49273b580386a3da14f60a985b45cebcd5
Sebastian Rodriguez <sebasrm2000@gmail.com>
2023-09-20 13:39:38 -0500

    estados independientes con useState

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 
+const SECURITY_CODE = 'paradigma';
+
 function UseState({ name }) {
-    const [error, setError] = React.useState(true);
+
+    const [value, setValue] = React.useState('');
+    const [error, setError] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
 
     React.useEffect(()=>{
         console.log("empezando efecto")
-        setTimeout(()=>{
-            console.log("Haciendo la validacion")
-            setLoading(false);
-            console.log("Terminando la validacion")
-        },3000)
-        
+        if(!!loading){
+            setTimeout(()=>{
+                console.log("Haciendo la validacion")
+                if(value!==SECURITY_CODE){
+                    setError(true)
+                }
+                
+                setLoading(false);
+                console.log("Terminando la validacion")
+            },3000)
+            
+        }
+     
         
         console.log("terminando efecto")
     },[loading]);
@@ -21,16 +32,25 @@ function UseState({ name }) {
             <h2>Eliminar {name}</h2>
             <p>Por favor, escribe el codigo de seguridad.</p>
 
-            {error && (
+            {(error && !loading) && (
                 <p>Error: el codigo es incorrecto</p>
             )}
             {loading && (
                 <p>Loading..</p>
             )}
 
-            <input placeholder="Codigo de seguridad" />
+            <input 
+
+            placeholder="Codigo de seguridad" 
+            value={value}
+            onChange={(event)=>{
+                setValue(event.target.value);
+
+            }}
+            />
             <button
-                onClick={() => setLoading(true)}
+                onClick={() => {
+                    setLoading(true)}}
             >Comprobar</button>
         </div>
     )
